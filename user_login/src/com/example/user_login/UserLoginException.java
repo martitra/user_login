cde35e771c7ca44cd658de1cf4d1a2bf0e93fa43
dam201 <dam201@PC2501.aulas.local>
2014-10-15 13:49:11 +0200

    Modificaciones

--- user_login/src/com/example/user_login/UserLoginException.java
+++ user_login/src/com/example/user_login/UserLoginException.java
@@ -16,4 +16,12 @@ public class UserLoginException extends Exception {
 		// TODO Auto-generated constructor stub
 	}
 
+
+	@Override
+	public String getMessage(){
+		//TODO Auto-generated method stub
+		return "Error User";
+	}
 }
+
+
--- user_login/src/com/example/user_login/UserLoginMainActivity.java
+++ user_login/src/com/example/user_login/UserLoginMainActivity.java
@@ -2,26 +2,48 @@ package com.example.user_login;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 
-public class MainActivity extends ActionBarActivity {
 
+public class UserLoginMainActivity extends ActionBarActivity {
+
+	private static final String LOGCAT="MainActivity";
+	EditText editLogin;
+	EditText editPassword;
+	UserLogin userLogin;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        editLogin = (EditText)findViewById(R.id.editText1);
+        editPassword = (EditText)findViewById(R.id.editText2);
     }
 
 
-    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
+    private void onClickButtonAcceder(View v){
+    	try{
+    		String login = editLogin.getText().toString();
+    		String password = editPassword.getText().toString();
+    		userLogin = new UserLogin(login, password);
+    		Log.i(LOGCAT, userLogin.toString());
+    	} catch (UserLoginException e){
+    		Log.e(LOGCAT, e.getMessage());
+    	}
+    }
+    
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
